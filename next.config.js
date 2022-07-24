394aea7511c0fcf3f9b2e5bf4c3941b604ae3318
ayumbro <ayumbro@gmail.com>
2022-07-24 23:00:52 +0800

    updated path and next config rewrite

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/stcss.less',
+        destination: '/api/stcss',
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
